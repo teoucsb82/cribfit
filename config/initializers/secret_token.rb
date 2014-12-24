e52c4394205ea4f422567f650e12e1e8133fba52
Teo Dell'Amico <teo@wanderable.com>
2014-12-24 09:05:01 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cribfit::Application.config.secret_key_base = 'c060dc43217cecc48f3be600be4598ee70bdf9ab194dec6e005e31a25d218394e4bc31787641a04b367a0478420b8fc4db27745c78dfa98cbe504f2b4f791d37'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cribfit::Application.config.session_store :cookie_store, key: '_cribfit_session'
